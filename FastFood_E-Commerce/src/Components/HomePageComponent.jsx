a3c1213b2d965838f48e4bb074e71b1d7fab9f4d
Sachidu-2001 <fsachi0001@gmail.com>
2025-04-09 02:46:19 +0200

    commit

---
+++ FastFood_E-Commerce/src/Components/HomePageComponent.jsx
@@ -0,0 +1,5 @@
+const HomePageComponent = () => {
+  return <></>;
+};
+
+export default HomePageComponent;
--- FastFood_E-Commerce/src/Components/NavBarComponent.jsx
+++ FastFood_E-Commerce/src/Components/NavBarComponent.jsx
@@ -1,30 +1,29 @@
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Container, Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NavBarComponent = () => {
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">FastFoodApp</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Link className="nav-link" to={"/"}>
+                Home
+              </Link>
+              <Link className="nav-link" to={"/prodotti"}>
+                I nostri prodotti
+              </Link>
+              <Link className="nav-link" to={"/novità"}>
+                Novità
+              </Link>
+              <Link className="nav-link" to={"/lavora con noi"}>
+                Lavora con noi
+              </Link>
             </Nav>
+            <Button variant="primary">LOGIN</Button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
