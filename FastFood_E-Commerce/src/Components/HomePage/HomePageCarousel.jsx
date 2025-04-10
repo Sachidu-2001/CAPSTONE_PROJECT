0435f45a45a28d3d6bcd83adc9871afd362e9c37
Sachidu-2001 <fsachi0001@gmail.com>
2025-04-10 08:59:46 +0200

    commit

--- FastFood_E-Commerce/src/Components/HomePage/HomePageCarousel.jsx
+++ FastFood_E-Commerce/src/Components/HomePage/HomePageCarousel.jsx
@@ -1,16 +1,40 @@
 import { Carousel } from "react-bootstrap";
+import ham1 from "../../assets/img/ham_carousel.jpg";
+import ham2 from "../../assets/img/hamImg.jpg";
+import ham3 from "../../assets/img/hamImg2.jpg";
+import ham4 from "../../assets/img/hamImg3.webp";
 
 const HomePageCarousel = () => {
   return (
     <Carousel>
-      <Carousel.Item interval={300}>
-        <h2>Prima Pagina</h2>
+      <Carousel.Item interval={700}>
+        <img className="d-block w-100 imgCar" src={ham1} alt="Carouselimg1" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={300}>
-        <h2>Seconda Pagina</h2>
+      <Carousel.Item interval={700}>
+        <img className="d-block w-100 imgCar" src={ham2} alt="Carouselimg1" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={300}></Carousel.Item>
-      <Carousel.Item interval={300}></Carousel.Item>
+      <Carousel.Item interval={700}>
+        <img className="d-block w-100 imgCar" src={ham3} alt="Carouselimg1" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={700}>
+        <img className="d-block w-100 imgCar" src={ham4} alt="Carouselimg1" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
     </Carousel>
   );
 };
