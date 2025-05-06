8ddc1efc58dd3a2f6cff6be1dc085ba96c7eef76
Sachidu-2001 <fsachi0001@gmail.com>
2025-05-06 16:44:33 +0200

    commit

--- FastFood_E-Commerce/src/Components/NotFound/NotFoundComponent.jsx
+++ FastFood_E-Commerce/src/Components/NotFound/NotFoundComponent.jsx
@@ -1,14 +1,19 @@
 import { Container } from "react-bootstrap";
+import notFoundImg from "../../assets/img/notfoundimg/ntfd.png";
 
 const NotFoundComponent = () => {
   return (
     <>
       <Container>
         <h1>Errore 404- Pagina Non Trovata</h1>
-        <h3>
-          La Pagina non è stata creata oppure il Tuo Programmatore è troppo
-          pigro
-        </h3>
+
+        <div className="d-flex flex-column align-items-center">
+          <img src={notFoundImg} alt="notFoundImg" style={{ width: "20em" }} />
+          <h3>
+            La pagina non è stata creata perchè...<br></br> Il programmatore era
+            troppo pigro
+          </h3>
+        </div>
       </Container>
     </>
   );
